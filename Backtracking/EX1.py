bfc852ec0dcb25d42db1eed6a93fc19ebef7860c
Parham Taheri | پرهام طاهری <62795984+parhamoyan@users.noreply.github.com>
2020-04-23 18:41:01 +0200

    Update EX1.py

--- Backtracking/EX1.py
+++ Backtracking/EX1.py
@@ -1,1 +1,20 @@
+def sous_ensembles(liste, current=0):
+    global choices
+    if current == 0:
+        choices = [True for i in range(len(liste))]
+    if current == len(liste):
+        s = "{ "
+        for i in range(len(liste)):
+            if choices[i]:
+                s += str(liste[i]) + " "
+        s += "}"
+        print(s)
+    else:
+        choices[current] = False
+        sous_ensembles(liste, current+1)
+        choices[current] = True
+        sous_ensembles(liste, current+1)
 
+# test :
+liste = [1,2,3,4]
+sous_ensembles(liste)
